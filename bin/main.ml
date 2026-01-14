1ea4fa25f0acedcaef39271dbe878d27894960f2
Thomas Volk <thomas.volk@trustedshops.de>
2026-01-14 13:58:53 +0100

    introduce stdin

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,8 @@
 open Lala
 
 let () =
-  let code = Sys.argv.(1) in
+  let code = match Array.to_list Sys.argv with
+  | [] | [_] -> In_channel.input_all In_channel.stdin
+  | _ :: h :: _ -> In_channel.with_open_text h (fun t -> In_channel.input_all t)
+  in
   print_endline (eval_code code)
-  
-
